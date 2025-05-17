af343d1246a059d1d09448c1151061f5b033cbad
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2025-05-17 12:14:14 +0200

    FIx the sqlite scripts

--- test/sqlite/parser_test.go
+++ test/sqlite/parser_test.go
@@ -8,16 +8,32 @@ import (
 )
 
 const (
-	schemaFileLocation = "data/schema_one_table.sql"
+	schemaOneTable  = "data/schema_one_table.sql"
+	schemaTwoTables = "data/schema_two_tables.sql"
 )
 
-// TestSqliteParser tests that Sqlize can parse a sqlite schema.
-func TestSqliteParser(t *testing.T) {
+// TestSqliteParser tests that Sqlize can parse a sqlite schema with one table.
+func TestParserSingleTable(t *testing.T) {
 	sqlizeCurrent := sqlize.NewSqlize(
 		sqlize.WithSqlite(),
 	)
 
-	schemaSqlBytes, err := os.ReadFile(schemaFileLocation)
+	schemaSqlBytes, err := os.ReadFile(schemaOneTable)
+	if err != nil {
+		t.Fatalf("failed to read schema file: %v", err)
+	}
+	if err := sqlizeCurrent.FromString(string(schemaSqlBytes)); err != nil {
+		t.Fatalf("failed to parse schema: %v", err)
+	}
+}
+
+// TestSqliteParser tests that Sqlize can parse a sqlite schema with foreign keys.
+func TestParserMultipleTables(t *testing.T) {
+	sqlizeCurrent := sqlize.NewSqlize(
+		sqlize.WithSqlite(),
+	)
+
+	schemaSqlBytes, err := os.ReadFile(schemaTwoTables)
 	if err != nil {
 		t.Fatalf("failed to read schema file: %v", err)
 	}
